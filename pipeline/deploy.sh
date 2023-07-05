21b84378fffe676a54303feaea0b2dbe54349fc9
lugbo <138655254+lugbo@users.noreply.github.com>
2023-07-05 10:10:20 +0000

    added mail and user

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,8 +1,14 @@
+#! /usr/bin/env sh
+
+set -e
+
 echo "executing $0 {"
 
 rm -rf bert_webserver/
 git clone https://$MACHINE_USER_TOKEN@github.com/lugbo/bert_webserver.git
 cd bert_webserver/
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 cp ../snippets/index.html .
 git add --all
 git commit -m "$(date)" || true
