6073edf12389fd354e66304bf9dad90dea5a8fe7
lugbo <138655254+lugbo@users.noreply.github.com>
2023-07-05 10:08:14 +0000

    added files

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,12 +1,12 @@
 echo "executing $0 {"
 
 rm -rf bert_webserver/
-git clone https://github.com/lugbo/bert_webserver.git
+git clone https://$MACHINE_USER_TOKEN@github.com/lugbo/bert_webserver.git
 cd bert_webserver/
 cp ../snippets/index.html .
 git add --all
-git commit -m "$(date)"
+git commit -m "$(date)" || true
 git push
-
-rm -rf ../bert_webserver/
+cd ..
+rm -rf bert_webserver/
 echo "done $0 }"
